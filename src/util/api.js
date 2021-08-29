bc92b9265426c562885ee5e17b0ad1a7f4962bc5
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-08-29 19:32:03 +0530

    u

--- src/util/api.js
+++ src/util/api.js
@@ -1,12 +1,12 @@
 const baseUrl =
-  process.env.NODE_ENV === 'development'
-    ? 'https://csb-88zdx-9rkd50xcm-sabarish.vercel.app/api'
+  process.env.NODE_ENV === "development"
+    ? "https://88zdx.sse.codesandbox.io/api"
     : `https://${window.location.hostname}/api`;
 
 const methods = {
   get: async function (endpoint, token = null) {
     const options = {
-      method: 'GET',
+      method: "GET",
       headers: {
         ...(token && { Authorization: `Bearer ${token}` })
       }
@@ -22,9 +22,9 @@ const methods = {
 
   post: async function (endpoint, body, token = null) {
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         ...(token && { Authorization: `Bearer ${token}` })
       },
       body: JSON.stringify(body)
@@ -35,7 +35,7 @@ const methods = {
 
     if (!response.ok) {
       if (response.status === 422) {
-        json.errors.forEach(error => {
+        json.errors.forEach((error) => {
           throw Error(`${error.param} ${error.msg}`);
         });
       }
@@ -48,9 +48,9 @@ const methods = {
 
   delete: async function (endpoint, token = null) {
     const options = {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         ...(token && { Authorization: `Bearer ${token}` })
       }
     };
@@ -59,7 +59,7 @@ const methods = {
     const json = await response.json();
 
     if (!response.ok) {
-      if (response.status === 401) throw Error('unauthorized');
+      if (response.status === 401) throw Error("unauthorized");
       throw Error(json.message);
     }
 
@@ -67,49 +67,49 @@ const methods = {
   }
 };
 
-export async function login (username, password) {
-  const json = await methods.post('login', { username, password });
+export async function login(username, password) {
+  const json = await methods.post("login", { username, password });
   return json.token;
 }
 
-export async function signup (username, password) {
-  const json = await methods.post('register', { username, password });
+export async function signup(username, password) {
+  const json = await methods.post("register", { username, password });
   return json.token;
 }
 
-export async function getPosts (category) {
+export async function getPosts(category) {
   return await methods.get(`posts/${category}`);
 }
 
-export async function getProfile (username) {
+export async function getProfile(username) {
   return await methods.get(`user/${username}`);
 }
 
-export async function getPost (id) {
+export async function getPost(id) {
   return await methods.get(`post/${id}`);
 }
 
-export async function createPost (body, token) {
-  return await methods.post('posts', body, token);
+export async function createPost(body, token) {
+  return await methods.post("posts", body, token);
 }
 
-export async function deletePost (id, token) {
+export async function deletePost(id, token) {
   return await methods.delete(`post/${id}`, token);
 }
 
-export async function createComment (post, comment, token) {
+export async function createComment(post, comment, token) {
   return await methods.post(`post/${post}`, comment, token);
 }
 
-export async function deleteComment (post, comment, token) {
+export async function deleteComment(post, comment, token) {
   return await methods.delete(`post/${post}/${comment}`, token);
 }
 
-export async function castVote (id, vote, token) {
+export async function castVote(id, vote, token) {
   const voteTypes = {
-    '1': 'upvote',
-    '0': 'unvote',
-    '-1': 'downvote'
+    "1": "upvote",
+    "0": "unvote",
+    "-1": "downvote"
   };
 
   const voteType = voteTypes[vote];
